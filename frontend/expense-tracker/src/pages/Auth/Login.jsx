f1f1f44cfc3d5963bf4c6d5ea58a31b26a8ca2c9
AdeepaS <adeepashashiprabhath@gmail.com>
2026-01-03 23:49:10 +0530

    login page

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { useState } from 'react';
 
 const Login = () => {
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [error,setError]=useState(null);
+
+  const navigate = useNavigate();
+
+ const handleLogin = async (e) => {} 
   return (
-    <div>
-      Login
-    </div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-\[5px] mb-6'>
+            Please enter your details to log in
+        </p>
+ 
+        <form onSubmit={handleLogin}>
+            <Input
+            value={email}
+            onChange={({e})=>setEmail(e.target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+            />
+            <Input
+            value={password}
+            onChange={({e})=>setPassword(e.target.value)}
+            label="Password"
+            placeholder=""
+            type="password"
+            />
+
+            
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
